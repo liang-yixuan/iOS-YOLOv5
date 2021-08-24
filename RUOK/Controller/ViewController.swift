a9a0eb1ce8d8186402bb45176a126a3858d59acb
Yixuan Liang <liangyixuan@SHORTs-MacBook-Pro.local>
2021-08-24 20:43:20 +1000

    finish landing page layout

---
+++ RUOK/Controller/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  RUOK
+//
+//  Created by SHORT on 24/8/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func aboutButtonClick(_ sender: UIButton) {
+        print("about button clicked")
+    }
+    
+    @IBAction func takePhotoButtonClick(_ sender: UIButton) {
+        print("take photo button clicked")
+    }
+    
+    @IBAction func choosePhotoButtonClick(_ sender: UIButton) {
+        print("choose photo button clicked")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
