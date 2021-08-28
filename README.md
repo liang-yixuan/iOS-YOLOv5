f2ea89c4ab87aa47bfe3b547dfb4a3b3786f24a8
Yixuan Liang <yixuanliang97@gmail.com>
2021-08-28 20:48:14 +1000

    camera function done

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# what
+# Yolov5 model in iOS
+
+This is a simple iOS application that takes a user selfie and predicts a person's identity.
+
+The models are trained from the Yolov5 model and are deployed on flask server.   
--- RUOK/Controller/ViewController.swift
+++ RUOK/Controller/ViewController.swift
@@ -6,24 +6,86 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
-    @IBAction func aboutButtonClick(_ sender: UIButton) {
-        print("about button clicked")
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let imagePicker1 = UIImagePickerController()
+    let imagePicker2 = UIImagePickerController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        imagePicker1.delegate = self
+        imagePicker1.sourceType = .camera
+        imagePicker1.allowsEditing = false
+        
+        imagePicker2.delegate = self
+        imagePicker2.sourceType = .photoLibrary
+        imagePicker2.allowsEditing = false
     }
     
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let pickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imageView.image = pickedImage
+            
+//            guard let ciimage = CIImage(image: pickedImage) else {
+//                fatalError("Could not convert to CI Image.")
+//            }
+//            detect(image: ciimage)
+            
+            
+        }
+        
+        imagePicker1.dismiss(animated: true, completion: nil)
+        imagePicker2.dismiss(animated: true, completion: nil)
+    }
+    
+//    func detect(image: CIImage) {
+//
+//            // Load the ML model through its generated class
+//        guard let model = try? VNCoreMLModel(for: yolov3(configuration: .init()).model) else {
+//                fatalError("can't load ML model")
+//            }
+//
+//            let request = VNCoreMLRequest(model: model) { request, error in
+//                guard let results = request.results as? [VNCoreMLFeatureValueObservation]//,
+////                    let topResult = results.first
+//                    else {
+//                        fatalError("unexpected result type from VNCoreMLRequest")
+//                }
+//                print(results)
+//            }
+//
+//            let handler = VNImageRequestHandler(ciImage: image)
+//
+//            do {
+//                try handler.perform([request])
+//            }
+//            catch {
+//                print(error)
+//            }
+//        }
+//
+
+        
+    
     @IBAction func takePhotoButtonClick(_ sender: UIButton) {
+        present(imagePicker1, animated: true, completion: nil)
         print("take photo button clicked")
     }
     
     @IBAction func choosePhotoButtonClick(_ sender: UIButton) {
+        present(imagePicker2, animated: true, completion: nil)
         print("choose photo button clicked")
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBAction func aboutButtonClick(_ sender: UIButton) {
+        print("about button clicked")
     }
 
 
